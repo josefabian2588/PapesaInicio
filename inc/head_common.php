88be7a1c467786059a252ed9886f690f33c50d99
Fabián Araya <josefabian2588@gmail.com>
2014-07-30 00:04:28 -0600

    tesx

---
+++ inc/head_common.php
@@ -0,0 +1,8 @@
+ <!-- Letras -->
+
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet">
+
+  
+
